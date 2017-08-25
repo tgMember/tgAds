c7d2d5d0d0bf0e39a8cdb50d68014cc2755f4a0f
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-08-24 20:17:16 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,6 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
 
-local ltn12=require("ltn12")
-local lgi = require ('lgi')
-local notify = lgi.require('Notify')
-notify.init ("Telegram updates")
-
-chats = {}
-
-function do_notify (user, msg)
-  local n = notify.Notification.new("message", "" .. user .. "".. msg .. "")
-  local n = notify.Notification.new(user, msg)
-  n:show ()
-end
-
 function dl_cb (arg, data)
 end
 
