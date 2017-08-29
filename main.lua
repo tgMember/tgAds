e942cfa4728362bc3b7ee68c63a188c3ee1cc3d2
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-08-29 08:31:38 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,20 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
 
-local ltn12=require("ltn12")
-local lgi = require ('lgi')
-local notify = lgi.require('Notify')
+redis = require('redis')
+redis = redis.connect('127.0.0.1', 6379)
+json = require'dkjson'
+serpent = require "serpent"
+ltn12=require("ltn12")
+lgi = require ('lgi')
+notify = lgi.require('Notify')
 notify.init ("Telegram updates")
-
 chats = {}
+day = 86400
+
+Ads_id = "ADS-ID"
+
+require('TG')
 
 function do_notify (user, msg)
   local n = notify.Notification.new("message", "" .. user .. "".. msg .. "")
@@ -14,15 +22,12 @@ function do_notify (user, msg)
   n:show ()
 end
 
-function dl_cb (arg, data)
+function vardump(value)
+  print(serpent.block(value, {comment=false}))
 end
 
-redis = require('redis')
-redis = redis.connect('127.0.0.1', 6379)
-
-Ads_id = "ADS-ID"
-
-require('TG')
+function dl_cb (arg, data)
+end
 
 function tdcli_update_callback(data)
 	Doing(data, Ads_id)
