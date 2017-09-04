92842f35f3b78ec2a25738bc8b00dfb05b16ae51
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-09-03 19:11:25 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,24 +3,12 @@ package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
 
 redis = require('redis')
 redis = redis.connect('127.0.0.1', 6379)
-json = require'dkjson'
 serpent = require "serpent"
 ltn12=require("ltn12")
-lgi = require ('lgi')
-notify = lgi.require('Notify')
-notify.init ("Telegram updates")
-chats = {}
-day = 86400
 
 Ads_id = "ADS-ID"
 
 require('TG')
-
-function do_notify (user, msg)
-  local n = notify.Notification.new("message", "" .. user .. "".. msg .. "")
-  local n = notify.Notification.new(user, msg)
-  n:show ()
-end
 
 function vardump(value)
   print(serpent.block(value, {comment=false}))
