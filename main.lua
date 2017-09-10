c6fe3b19a7835c2fc8900cec5e6cebf90a78f310
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-09-10 09:21:25 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,12 @@
-package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
-package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
+package.path = package.path..';../.luarocks/share/lua/5.2/?.lua;../.luarocks/share/lua/5.2/?/init.lua'
+package.cpath = package.cpath..';../.luarocks/lib/lua/5.2/?.so'
 
 redis = require('redis')
 redis = redis.connect('127.0.0.1', 6379)
-serpent = require "serpent"
-ltn12=require("ltn12")
 
 Ads_id = "ADS-ID"
 
 require('TG')
-
-function vardump(value)
-  print(serpent.block(value, {comment=false}))
-end
 
 function dl_cb (arg, data)
 end
